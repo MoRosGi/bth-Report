a6876cbecebf88b220f5e32b9d83c52cff284f65
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-04-13 21:49:19 +0200

    Add api/[card game fuctionalities]

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class ApiController extends AbstractController
 {
@@ -12,5 +15,31 @@ class ApiController extends AbstractController
     public function api(): Response
     {
         return $this->render('api.html.twig');
+    }
+
+
+    #[Route("/api/quote", name: "quote")]
+    public function quote(): Response
+    {
+        $quote = ["Either you run the day or the day runs you.", "What happens is not as important as how you react to what happens.", "Poor eyes limit your sight; poor vision limits your deeds.", "The only true wisdom is in knowing you know nothing."];
+        $randomQuote = array_rand($quote);
+        $quoteDay = $quote[$randomQuote];
+
+        $currentDate = date('Y-m-d');
+        date_default_timezone_set('Europe/Stockholm');
+        $timestamp = date("h:i:sa");
+
+        $data = [
+            'Date of today:' => $currentDate,
+            'Quote for today:' => $quoteDay,
+            'Quote generated at:' => $timestamp
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+
+        return $response;
     }
 }
