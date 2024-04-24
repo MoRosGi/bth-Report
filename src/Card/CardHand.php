211197c2ea76f54232e5a9ce5a8890338889aa8c
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-04-24 17:59:06 +0200

    Add handStringLetter method

--- src/Card/CardHand.php
+++ src/Card/CardHand.php
@@ -6,7 +6,11 @@ use App\Card\Card;
 
 class CardHand
 {
-    private $hand;
+    /**
+    * @var array<Card> $hand
+    */
+    private array $hand = [];
+
 
     public function addCard(Card $card): void
     {
@@ -14,12 +18,18 @@ class CardHand
     }
 
 
+    /**
+    * @return array<Card> $hand
+    */
     public function getHand(): array
     {
         return $this->hand;
     }
 
 
+    /**
+    * @return array<string> $values
+    */
     public function handString(): array
     {
         $values = [];
@@ -28,5 +38,41 @@ class CardHand
         }
 
         return $values;
+    }
+
+
+    /**
+    * @return array<string> $values
+    */
+    public function handStringLetter(): array
+    {
+        $values = [];
+        foreach ($this->hand as $card) {
+            $values[] = $card->cardStringLetter();
+        }
+
+        return $values;
+    }
+
+
+    public function handValue(): int
+    {
+        $totalValue = 0;
+        $aceCount = 0;
+
+        foreach ($this->hand as $card) {
+            $totalValue += $card->cardValue($card);
+
+            if ($card->getValue() === 'Ace') {
+                $aceCount++;
+            }
+        }
+
+        while ($totalValue > 21 && $aceCount > 0) {
+            $totalValue -= 13;
+            $aceCount--;
+        }
+
+        return $totalValue;
     }
 }
