a73a1264019e94eaaadf5b9a45dfeaa132579909
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-05-10 23:37:48 +0200

    Add sql files for insert and backup database

---
+++ app/sql/backup.sql
@@ -0,0 +1,16 @@
+DROP TABLE book;
+
+CREATE TABLE IF NOT EXISTS book (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    title VARCHAR(255) NOT NULL, isbn VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    imagePath VARCHAR(255) DEFAULT NULL
+);
+
+INSERT INTO book
+    (title, isbn, author, imagePath)
+VALUES
+    ('Elephants Can Remember', '9780002312202', 'Agatha Christie', 'https://pictures.abebooks.com/isbn/9780002312202-uk.jpg'),
+    ('Void Moon', '0752821385', 'Michael Connelly', 'https://pictures.abebooks.com/isbn/9780752821382-uk.jpg'),
+    ('Walkin the Dog', '1852426500 ', 'Walter Mosley', 'https://pictures.abebooks.com/inventory/22660540583.jpg')
+;
---
+++ app/sql/insert.sql
@@ -0,0 +1,9 @@
+DELETE FROM book;
+
+INSERT INTO book
+    (title, isbn, author, imagePath)
+VALUES
+    ('Elephants Can Remember', '9780002312202', 'Agatha Christie', 'https://pictures.abebooks.com/isbn/9780002312202-uk.jpg'),
+    ('Void Moon', '0752821385', 'Michael Connelly', 'https://pictures.abebooks.com/isbn/9780752821382-uk.jpg'),
+    ('Walkin the Dog', '1852426500 ', 'Walter Mosley', 'https://pictures.abebooks.com/inventory/22660540583.jpg')
+;
