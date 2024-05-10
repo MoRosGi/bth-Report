90aa7af6e6ce5d8c0de3a40682c9fbbbc69665f6
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-05-10 23:35:10 +0200

    Update controller with kmom05 library routes

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -7,6 +7,9 @@ use App\Card\CardDeck;
 use App\Card\Game;
 use App\TwentyOne\GamePlay;
 
+use App\Repository\BookRepository;
+// use Doctrine\Persistence\ManagerRegistry;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -295,6 +298,37 @@ class ApiController extends AbstractController
         ];
 
         $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+
+        return $response;
+    }
+
+    #[Route('/api/library/books', name: 'api_library', methods: ['GET'])]
+    public function index(
+        BookRepository $bookRepository
+    ): Response {
+        $books = $bookRepository
+            ->findAll();
+
+        $response = $this->json($books);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+
+        return $response;
+    }
+
+    #[Route('/product/show/{isbn}', name: 'api_library_isbn', methods: ['GET'])]
+    public function showProductById(
+        BookRepository $bookRepository,
+        string $isbn
+    ): Response {
+        $book = $bookRepository
+            ->findIsbn($isbn);
+
+        $response = $this->json($book);
         $response->setEncodingOptions(
             $response->getEncodingOptions() | JSON_PRETTY_PRINT
         );
