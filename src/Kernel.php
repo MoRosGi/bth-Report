3e2245bae0f0233f7318feca19575b7ffcede7da
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-06-25 18:49:19 +0200

    Update config files

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,16 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        /**
+         * @var string $timezone
+         */
+        $timezone = $this->getContainer()->getParameter('timezone');
+
+        date_default_timezone_set($timezone);
+    }
 }
